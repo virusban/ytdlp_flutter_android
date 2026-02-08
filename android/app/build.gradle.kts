2a3358422c077c9d5d66bb71052fd4e43673309a
virusban <ron.minecraft2022@gmail.com>
2026-02-08 08:16:54 +0200

    started project from zero (6 update)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,20 +1,14 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
-    id("com.chaquo.python")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.ronna.ytdlpflutterandroid"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.ronna.ytdlpflutterandroid"
-        minSdk = 30
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
+    namespace = "com.example.ytdlp_flutter_android"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -22,19 +16,29 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.ytdlp_flutter_android"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
 }
 
-python {
-    buildPython("3.10")
-    pip {
-        install("yt-dlp")
-        install("ffmpeg-python")
-    }
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
-    implementation("androidx.core:core-ktx:1.12.0")
+flutter {
+    source = "../.."
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,24 @@
-buildscript {
-    val kotlin_version by extra("1.9.10")
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("com.chaquo.python:gradle:15.0.0")
-    }
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
